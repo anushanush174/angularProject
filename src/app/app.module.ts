93fc693cbf8dca05525ef12b09fea7a0fa390563
AnushMelkonyan <melkonyananushbk@gmail.com>
2020-10-06 16:32:55 +0400

    end 4th folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,26 @@
+import { StyleDirective } from './directives/style.directive';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldListComponent } from './hello-world-list/hello-world-list.component';
-import { LoginComponentComponent } from './log-register/login-component/login-component.component';
-import { RegistrationComponentComponent } from './log-register/registration-component/registration-component.component';
-
 import {ReactiveFormsModule} from '@angular/forms'
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldListComponent,
-    LoginComponentComponent,
-    RegistrationComponentComponent,
+    StyleDirective,
+
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    
   ],
+
   providers: [],
-  exports: [HelloWorldListComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
