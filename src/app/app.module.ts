0bdc7b84e525c1a2e7c42cbb957015fa7ab984a7
AnushMelkonyan <melkonyananushbk@gmail.com>
2020-10-06 13:39:17 +0400

    clean project for starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldListComponent } from './hello-world-list/hello-world-list.component';
-import { LoginComponentComponent } from './log-register/login-component/login-component.component';
-import { RegistrationComponentComponent } from './log-register/registration-component/registration-component.component';
-
 import {ReactiveFormsModule} from '@angular/forms'
 import {FormsModule} from '@angular/forms'
 
@@ -14,19 +9,15 @@ import {FormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldListComponent,
-    LoginComponentComponent,
-    RegistrationComponentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
     
   ],
   providers: [],
-  exports: [HelloWorldListComponent],
+  exports: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
