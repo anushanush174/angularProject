2fc30329a4f1efef05fbb5b836f4f75dd08c42c5
AnushMelkonyan <melkonyananushbk@gmail.com>
2020-10-07 13:48:03 +0400

    end pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string,
+  text: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  e: number = Math.E
+
+  search = ""
+  posts: Post[] = [
+    {title: 'beer', text: 'the best beer in the world'},
+    {title: 'bread', text: 'the best bread in the world'},
+    {title: 'Javascript', text: 'the best language in the world'},
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {ReactiveFormsModule} from '@angular/forms'
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import { FilterPipe } from './pipes/filter.pipe'
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    FilterPipe,
   ],
 
   imports: [
