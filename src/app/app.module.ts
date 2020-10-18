8ac53126716a84b2a32409930fbf0577ce833927
AnushMelkonyan <melkonyananushbk@gmail.com>
2020-10-18 22:57:27 +0400

    end 10th folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
 
-import { AppComponent } from './app.component';
-import {ReactiveFormsModule} from '@angular/forms'
+import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms'
-
+import {HomePageComponent} from './home-page/home-page.component'
+import {AppRoutingModule} from './app-routing.module'
+import { SharedModule } from './shared/shared.module'
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent,  //components, directives, pipes
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
     FormsModule,
-    
+    AppRoutingModule, 
+    SharedModule //modules
   ],
-  providers: [],
-  exports: [],
-  bootstrap: [AppComponent]
+  providers: [], //services
+  bootstrap: [AppComponent] // main module
 })
-export class AppModule { } 
+export class AppModule {
+}
